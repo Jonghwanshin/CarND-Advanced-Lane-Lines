5a86ad7d10c2682e3f58565f7330d3b5d0eef39a
Jonghwan Shin <jhshin@hyunbo.com>
2020-12-30 17:54:01 +0900

    20201230 add new image for thresholding

--- LaneBoundary.py
+++ LaneBoundary.py
@@ -1,4 +1,9 @@
 import numpy as np
+from sklean import linear_model
+
+def add_square_feature(X):
+    X = np.concatenate([(X**2).reshape(-1,1), X], axis=1)
+    return X
 
 class LaneBoundary:
     def __init__(self, xpoints, ypoints):
@@ -32,10 +37,19 @@ class LaneBoundary:
     def location(self):
         return self.__location
 
-    def fit_point(self):
+    @property
+    def func(self):
+        return self.__func
+
+    def fit_point(self, ):
         try:
             # Fit a second order polynomial to each using `np.polyfit`
             self.__fit_coeffs = np.polyfit(self.points_y, self.points_x, 2)
+
+            # Robustly fit linear model with RANSAC algorithm
+            # ransac = linear_model.RANSACRegressor()
+            # ransac.fit(add_square_feature(self.points_y), self.points_x)
+            self.__func = np.poly1d(self.__fit_coeffs)
             return True
         except Exception as inst:
             print(self.points_x.shape)
@@ -47,5 +61,4 @@ class LaneBoundary:
         """
             Calculate the location at bottom of the image
         """
-        
-        self.location = loc
+        self.location = self.func(0)
